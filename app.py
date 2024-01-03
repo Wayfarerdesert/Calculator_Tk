9802d80b21e67462d3b01b72e4e6e05f85979b97
Wayfarerdesert <125356747+Wayfarerdesert@users.noreply.github.com>
2024-01-03 14:19:11 +0100

    functional calculator working

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from tkinter import *  # noqa: F403
+import ast
 
 root = Tk()
 root.title("Calculator")
@@ -14,14 +15,17 @@ def getNumbers(n):
     display.insert(index, n)
     index += 1
 
+
 def getOperator(operator):
     global index
     operator_length = len(operator)
     display.insert(index, operator)
     index += operator_length
 
+
 def clearDisplay():
     display.delete(0, END)
+
 
 def undo():
     display_state = display.get()
@@ -32,44 +36,78 @@ def undo():
     else:
         clearDisplay()
 
+
 def calculate():
-    pass
+    display_state = display.get()
+    try:
+        math_expression = ast.parse(display_state, mode="eval")
+        # result = ast.dump(math_expression)
+        result = eval(compile(math_expression, "<string>", "eval"))
+        clearDisplay()
+        display.insert(0, result)
+    except Exception as e:
+        clearDisplay()
+        display.insert(0, f"Error: {e}")
+
 
 # Operations Buttons
-Button(root, text="x²", command=lambda: getOperator("**")).grid(row=1, column=1, sticky=W + E)
-Button(root, text="√", command=lambda: getOperator("**0.5")).grid(row=1, column=2, sticky=W + E)
-Button(root, text="←", command=lambda: undo()).grid(row=1, column=4, columnspan=2, sticky=W + E)
+Button(root, text="x²", command=lambda: getOperator("**")).grid(
+    row=1, column=1, sticky=W + E)
+Button(root, text="√", command=lambda: getOperator("**0.5")).grid(
+    row=1, column=2, sticky=W + E)
+Button(root, text="←", command=lambda: undo()).grid(
+    row=1, column=4, columnspan=2, sticky=W + E)
 
-Button(root, text="%", command=lambda:getOperator("%")).grid(row=2, column=0, sticky=W + E)
-Button(root, text="÷", command=lambda:getOperator("/")).grid(row=2, column=1, sticky=W + E)
-Button(root, text="x", command=lambda:getOperator("*")).grid(row=2, column=2, sticky=W + E)
+Button(root, text="%", command=lambda: getOperator("%")).grid(
+    row=2, column=0, sticky=W + E)
+Button(root, text="÷", command=lambda: getOperator("/")).grid(
+    row=2, column=1, sticky=W + E)
+Button(root, text="x", command=lambda: getOperator("*")).grid(
+    row=2, column=2, sticky=W + E)
 
-Button(root, text="-", command=lambda:getOperator("-")).grid(row=2, column=4, sticky=W + E)
-Button(root, text="+", command=lambda:getOperator("+")).grid(row=3, column=4, sticky=W + E, rowspan=2)
-Button(root, text="=").grid(row=5, column=4, sticky=W + E, rowspan=2)
+Button(root, text="-", command=lambda: getOperator("-")).grid(
+    row=2, column=4, sticky=W + E)
+Button(root, text="+", command=lambda: getOperator("+")).grid(
+    row=3, column=4, sticky=W + E, rowspan=2)
+Button(root, text="=", command=lambda: calculate()).grid(
+    row=5, column=4, sticky=W + E, rowspan=2)
 
-Button(root, text=",", command=lambda:getOperator(",")).grid(row=6, column=2, sticky=W + E)
+Button(root, text=",", command=lambda: getOperator(",")).grid(
+    row=6, column=2, sticky=W + E)
 
 # Numeric Buttons
-Button(root, text="1", command=lambda: getNumbers(1)).grid(row=3, column=0, sticky=W + E)
-Button(root, text="2", command=lambda: getNumbers(2)).grid(row=3, column=1, sticky=W + E)
-Button(root, text="3", command=lambda: getNumbers(3)).grid(row=3, column=2, sticky=W + E)
+Button(root, text="1", command=lambda: getNumbers(1)).grid(
+    row=5, column=0, sticky=W + E)
+Button(root, text="2", command=lambda: getNumbers(2)).grid(
+    row=5, column=1, sticky=W + E)
+Button(root, text="3", command=lambda: getNumbers(3)).grid(
+    row=5, column=2, sticky=W + E)
 
-Button(root, text="4", command=lambda: getNumbers(4)).grid(row=4, column=0, sticky=W + E)
-Button(root, text="5", command=lambda: getNumbers(5)).grid(row=4, column=1, sticky=W + E)
-Button(root, text="6", command=lambda: getNumbers(6)).grid(row=4, column=2, sticky=W + E)
+Button(root, text="4", command=lambda: getNumbers(4)).grid(
+    row=4, column=0, sticky=W + E)
+Button(root, text="5", command=lambda: getNumbers(5)).grid(
+    row=4, column=1, sticky=W + E)
+Button(root, text="6", command=lambda: getNumbers(6)).grid(
+    row=4, column=2, sticky=W + E)
 
-Button(root, text="7", command=lambda: getNumbers(7)).grid(row=5, column=0, sticky=W + E)
-Button(root, text="8", command=lambda: getNumbers(8)).grid(row=5, column=1, sticky=W + E)
-Button(root, text="9", command=lambda: getNumbers(9)).grid(row=5, column=2, sticky=W + E)
+Button(root, text="7", command=lambda: getNumbers(7)).grid(
+    row=3, column=0, sticky=W + E)
+Button(root, text="8", command=lambda: getNumbers(8)).grid(
+    row=3, column=1, sticky=W + E)
+Button(root, text="9", command=lambda: getNumbers(9)).grid(
+    row=3, column=2, sticky=W + E)
 
-Button(root, text="0", command=lambda: getNumbers(0)).grid(row=6, column=0, columnspan=2, sticky=W + E)
+Button(root, text="0", command=lambda: getNumbers(0)).grid(
+    row=6, column=0, columnspan=2, sticky=W + E)
 
 # Side Buttons
 Button(root, text="C").grid(row=2, column=5, columnspan=2, sticky=W + E)
-Button(root, text="AC", command=lambda: clearDisplay()).grid(row=3, column=5, columnspan=2, sticky=W + E)
-Button(root, text="(", command=lambda:getOperator("(")).grid(row=4, column=5, columnspan=2, sticky=W + E)
-Button(root, text=")", command=lambda:getOperator(")")).grid(row=5, column=5, columnspan=2, sticky=W + E)
+Button(root, text="AC", command=lambda: clearDisplay()).grid(
+    row=3, column=5, columnspan=2, sticky=W + E)
+Button(root, text="(", command=lambda: getOperator("(")).grid(
+    row=4, column=5, columnspan=2, sticky=W + E)
+Button(root, text=")", command=lambda: getOperator(")")).grid(
+    row=5, column=5, columnspan=2, sticky=W + E)
 Button(root, text="+/-").grid(row=6, column=5, sticky=W + E)
 
 root.mainloop()
